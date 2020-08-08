a9b61161454a7199df6813beeba0f2ce19a1c83e
jack mck <jackmck62@gmail.com>
2020-08-08 15:53:35 +0100

    Changed some stuff.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name...");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+            Console.WriteLine($"Press any key to exit.");
+            Console.ReadKey(true);
         }
     }
 }
